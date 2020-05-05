54583f27c7c96e0efe35f882ea8d03ff81b4c9d8
Thomas Shehan <thomas.shehan88@gmail.com>
2020-05-05 15:44:26 -0400

    adjusted text colors

--- game/HUD.gd
+++ game/HUD.gd
@@ -2,5 +2,5 @@ extends Control
 
 func no_cloak():
 	$Cloak.text = "Cloak Unavailable"
-	$Cloak.add_color_override("font_color", Color(0.6, 0.3, 0.3))
+	$Cloak.add_color_override("font_color", Color(0.8, 0.4, 0))
 	
