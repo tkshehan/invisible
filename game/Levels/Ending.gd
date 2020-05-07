f5d35a251a847ea1527db40ba81c11cf35c7aae9
Thomas Shehan <thomas.shehan88@gmail.com>
2020-05-06 23:48:23 -0400

    fix urls not displaying in credits

--- game/Levels/Ending.gd
+++ game/Levels/Ending.gd
@@ -6,10 +6,3 @@ func _ready() -> void:
 func ending_chime():
 	globals.stop_music()
 	$AudioStreamPlayer.fade_in()
-
-func _on_github_pressed() -> void:
-	OS.shell_open("https://github.com/tkshehan")
-
-
-func _on_soundcloud_pressed() -> void:
-	OS.shell_open("https://soundcloud.com/horofosho")
